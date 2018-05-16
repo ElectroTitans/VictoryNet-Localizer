c706a6d3b7a8a607ebca30194b2245fec8c1cd9d
Alex Carter <accounts@atecstudios.com>
2018-05-15 22:20:07 -0700

    Fixed Pip Requiremtns

--- setup.sh
+++ setup.sh
@@ -2,15 +2,14 @@
 sudo apt-get install zip -y
 sudo apt-get install unzip -y
 sudo apt-get -y install python3-pip
-sudo pip3 install .
+sudo pip3 install -r requirements.txt 
 
 file="google-cloud-sdk-101.0.0-linux-x86_64.tar.gz"
 link="https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/"
 
 curl -L "$link""$file" | tar xz 
 CLOUDSDK_CORE_DISABLE_PROMPTS=1 ./google-cloud-sdk/install.sh
-exec -l $SHELL
-gcloud init
+
 gcloud auth activate-service-account --key-file gcpkey.json
 mkdir Data/
 
